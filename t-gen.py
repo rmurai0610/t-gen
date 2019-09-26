0e2ba0e271e41ca3a09322719ebc8ad3f5699b11
rmurai0610 <rmurai0610@gmail.com>
2019-09-26 22:09:10 +0100

    Join path properly

--- t-gen.py
+++ t-gen.py
@@ -2,11 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import os, sys, re, shutil
+def copy_no_override(src, dst):
+    if os.path.exists(dst):
+        # if file exists, do not copy
+        return
+    shutil.copy(src, dst)
+
 
 def main():
     if len(sys.argv) == 1 or '-h' in sys.argv:
-        print('-h: Display help')
+        print('-h                      Displays help')
+        print('./t-gen.py ABC001       Infers the number of questions from the contest name')
+        print('./t-gen.py ABC001 ABCD  Manually specify the number of questions')
         return
+
     contest_name = ''
     contest_number = ''
     contest_questions = []
@@ -27,21 +36,20 @@ def main():
         # Check for contest name + number + questions
         contest_name, contest_number = [i for i in re.split(r'(\d+)', sys.argv[1]) if i]
         contest_questions = list(sys.argv[2])
-    print(contest_name, contest_number, contest_questions)
 
 
-    # Generate directory
+    # Set up directory
     root_dir = os.path.expanduser('~/project/atcoder')
-    contest_dir = root_dir + '/' + contest_name + contest_number
+    template_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates')
+    contest_dir = os.path.join(root_dir, contest_name + contest_number)
     os.makedirs(contest_dir, exist_ok=True)
-    # Create cpp files
-    for contest_question in contest_questions:
-        open(contest_dir + '/' + contest_question + '.cc', 'a').close()
-    # Copy template files into directory
-    template_dir = os.path.dirname(os.path.realpath(__file__)) + '/templates'
-    for template_file in ['CMakeLists.txt', 'Makefile', 'test.sh']:
-        shutil.copy(template_dir + '/' + template_file, contest_dir + '/' + template_file)
 
+    # Copy template cpp files
+    for contest_question in contest_questions:
+        copy_no_override(os.path.join(template_dir, 'template.cc'), os.path.join(contest_dir, contest_question + '.cc'))
+    # Copy template files into directory
+    for template_file in ['CMakeLists.txt', 'Makefile', 'test.sh']:
+        copy_no_override(os.path.join(template_dir, template_file), os.path.join(contest_dir, template_file))
 
 if __name__ == '__main__':
     main()
